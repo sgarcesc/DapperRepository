730ce174c224cfe9a6ffec45379a6dca8fd97a45
Sebastian Garces <sgarcesc@gmail.com>
2018-03-08 14:02:56 -0500

    CompositionRoot

---
+++ DapperRepository/CompositionRoot.cs
@@ -0,0 +1,13 @@
+﻿using LightInject;
+
+namespace DapperRepository
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry service)
+        {
+            service.Register(typeof(IRepository<>), typeof(Repository<>));
+            service.RegisterAssembly(GetType().Assembly, (serviceType, implementingType) => serviceType.Name.EndsWith("Repository"));
+        }
+    }
+}
